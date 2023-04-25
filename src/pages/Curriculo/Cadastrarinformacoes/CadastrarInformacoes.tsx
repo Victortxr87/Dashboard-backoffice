53ca8d921dd99b21202ad4b498ddf53a3e8c3e65
Victortxr87 <82127678+Victortxr87@users.noreply.github.com>
2023-04-26 00:10:14 +0100

    atualizando funcionalidades

--- src/pages/Curriculo/Cadastrarinformacoes/CadastrarInformacoes.tsx
+++ src/pages/Curriculo/Cadastrarinformacoes/CadastrarInformacoes.tsx
@@ -2,21 +2,19 @@
 import styles from "../../../components/forms/Input/Input.module.css";
 import Input from "../../../components/forms/Input/Input";
 import Textarea from "../../../components/forms/textarea/textarea";
+import { Informacoes, createInfomacoes } from "../../../services/informacoesServices";
+
 import * as Yup from "yup";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form  } from "formik";
 
 
 
-interface FormValues {
-    foto: string;
-    nome: string;
-    cargo: string;
-    resumo: string;
-};
+
 
 const CadastrarInformacoes: React.FC = () => {
 
-    const initialValues: FormValues = {
+    const initialValues: Informacoes = {
+        id: 1,
         foto: '',
         nome: '',
         cargo: '',
@@ -30,11 +28,28 @@ const CadastrarInformacoes: React.FC = () => {
         resumo: Yup.string().required('Campo obrigatório')
     });
 
-    const onSubmit = (values: FormValues, { resetForm }: { resetForm: () => void }) => {
-        //Lógica de envio para backend
-        console.log(values);
-        resetForm();
-        alert('Formulário enviado com sucesso!');
+    const onSubmit = async (values: Informacoes, { resetForm }: { resetForm: () => void }) => {
+        
+
+
+
+        try {
+            await createInfomacoes(values);
+           
+            console.log(values);
+            // resetForm();
+            alert('Formulário enviado com sucesso!');
+
+        } catch (error) {
+
+            console.error('Erro ao enviar formulário', error);
+            alert('Erro ao enviar formulário. Tente novamente.');
+
+        }
+        // //Lógica de envio para backend
+        // console.log(values);
+        // resetForm();
+        // alert('Formulário enviado com sucesso!');
         
     };
 
---
+++ src/services/informacoesServices.ts
@@ -0,0 +1,15 @@
+import api from './api';
+
+export interface Informacoes {
+    id: number;
+    foto: string;
+    nome: string;
+    cargo: string;
+    resumo: string;
+}
+
+export async function createInfomacoes(informacoes: Informacoes): Promise<Informacoes> {
+    const response = await api.put<Informacoes>('/informacoes/1', informacoes);
+    return response.data;
+}
+
